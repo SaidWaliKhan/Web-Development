591b25cb9abef00bc3481b1545a92b70e635b9b0
ayyankhan <135837847+SaidWaliKhan@users.noreply.github.com>
2025-08-05 06:32:54 -0700

    Add files via upload

---
+++ React_Js/Meme_Generator_App/Meme-Generator/src/Components/Head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <header className='header'>
+        <img src="./src/assets/Images/Troll Face.png" alt="" 
+        className='header-img'/>
+        <h2 className='header-title'>Meme Generator</h2>
+        <h3 className='header-project'>React Course - Project 3</h3>
+    </header>
+  )
+}
---
+++ React_Js/Meme_Generator_App/Meme-Generator/src/Components/MemesData.jsx
@@ -0,0 +1,92 @@
+const MemesData = {
+  success: true,
+  data: {
+    memes: [
+      {
+        id: "181913649",
+        name: "Drake Hotline Bling",
+        url: "https://i.imgflip.com/1o00in.jpg",
+        width: 1200,
+        height: 1200,
+        box_count: 2,
+      },
+      {
+        id: "186542580",
+        name: "Mocking SpongeBob",
+        url: "https://i.imgflip.com/1otk96.jpg",
+        width: 800,
+        height: 800,
+        box_count: 3,
+      },
+      {
+        id: "134797575",
+        name: "Expanding Brain",
+        url: "https://i.imgflip.com/1jwhww.jpg",
+        width: 1200,
+        height: 1200,
+        box_count: 4,
+      },
+      {
+        id: "112701428",
+        name: "Two Buttons",
+        url: "https://i.imgflip.com/1g8my4.jpg",
+        width: 1000,
+        height: 1000,
+        box_count: 5,
+      },
+      {
+        id: "112126428",
+        name: "Distracted Boyfriend",
+        url: "https://i.imgflip.com/1ur9b0.jpg",
+        width: 1200,
+        height: 1200,
+        box_count: 6,
+      },
+      {
+        id: "110590491",
+        name: "Roll Safe",
+        url: "https://i.imgflip.com/1h7in3.jpg",
+        width: 800,
+        height: 800,
+        box_count: 7,
+      },
+      {
+        id: "162902256",
+        name: "Surprised Pikachu",
+        url: "https://i.imgflip.com/1h7in3.jpg",
+        width: 800,
+        height: 800,
+        box_count: 8,
+      },
+
+      {
+        id: "154880",
+        name: "Mocking SpongeBob",
+        url: "https://i.imgflip.com/1otk96.jpg",
+        width: 800,
+        height: 800,
+        box_count: 2,
+      },
+
+      {
+        id: "61520",
+        name: "One Does Not Simply",
+        url: "https://i.imgflip.com/1bij.jpg",
+        width: 640,
+        height: 640,
+        box_count: 2,
+      },
+
+      {
+        id: "219492",
+        name: "Expanding Brain",
+        url: "https://i.imgflip.com/1jwhww.jpg",
+        width: 1200,
+        height: 1200,
+        box_count: 4,
+      },
+    ],
+  },
+};
+
+export default MemesData;
