f2dd67b0aedba4e98e865c9e5e6c16c1b5114a16
ayyankhan <135837847+SaidWaliKhan@users.noreply.github.com>
2025-08-05 06:27:45 -0700

    Add files via upload

---
+++ React_JS/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from './Components/Head'
+import Meme from './Components/Meme'
+
+export default function App() {
+  return (
+    <div>
+      <Head/>
+      <Meme/>
+    </div>
+  )
+}
